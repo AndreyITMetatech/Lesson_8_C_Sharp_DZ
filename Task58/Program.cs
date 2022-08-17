3eada048a1dfbd08ada72c16a06c8aff85e58aef
Andrey <C:\Users\Andrey\AppData\Roaming\The Bat!>
2022-08-17 22:25:18 +0300

    Добавил решения

---
+++ Task58/Program.cs
@@ -0,0 +1,69 @@
+﻿// Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+
+Console.WriteLine($"\nВведите размеры матриц и диапазон случайных значений: ");
+int m = InputNumbers("Введите число строк матрицы 1: ");
+int n = InputNumbers("Введите число столбцов матрицы 1: ");
+int p = InputNumbers("Введите число столбцов матрицы 2: ");
+int range = InputNumbers("Введите диапазон случайных чисел: от 1 ");
+
+int[,] firstMartrix = new int[m, n];
+CreateArray(firstMartrix);
+Console.WriteLine($"\nМатрица 1:");
+WriteArray(firstMartrix);
+
+int[,] secomdMartrix = new int[n, p];
+CreateArray(secomdMartrix);
+Console.WriteLine($"\nМатрица 2:");
+WriteArray(secomdMartrix);
+
+int[,] resultMatrix = new int[m,p];
+
+MultiplyMatrix(firstMartrix, secomdMartrix, resultMatrix);
+Console.WriteLine($"\nПроизведение обеих матриц:");
+WriteArray(resultMatrix);
+
+void MultiplyMatrix(int[,] firstMartrix, int[,] secomdMartrix, int[,] resultMatrix)
+{
+  for (int i = 0; i < resultMatrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < resultMatrix.GetLength(1); j++)
+    {
+      int sum = 0;
+      for (int k = 0; k < firstMartrix.GetLength(1); k++)
+      {
+        sum += firstMartrix[i,k] * secomdMartrix[k,j];
+      }
+      resultMatrix[i,j] = sum;
+    }
+  }
+}
+
+int InputNumbers(string input)
+{
+  Console.Write(input);
+  int output = Convert.ToInt32(Console.ReadLine());
+  return output;
+}
+
+void CreateArray(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      array[i, j] = new Random().Next(range);
+    }
+  }
+}
+
+void WriteArray (int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      Console.Write(array[i,j] + " ");
+    }
+    Console.WriteLine();
+  }
+}
---
+++ Task62/Program.cs
@@ -0,0 +1,64 @@
+﻿// Задача 62. Заполните спирально массив 4 на 4.
+
+Console.WriteLine("Введите длину массива: ");
+int N = int.Parse(Console.ReadLine()!);
+
+int[,] matrix = new int[N, N];
+int a = 1;
+int i = 0;
+int j = 0;
+int count = 0;
+
+while (matrix[(i + 1), j] == 0 || matrix[(i - 1), j] == 0 || matrix[i, (j + 1)] == 0 || matrix[i, (j - 1)] == 0 || matrix[i, j] == 0)
+{
+    while (i < (N - count))
+    {
+        matrix[i, j] = a;
+        a++;
+        i++;
+    }
+    i--;
+    j++;
+
+    while (j < (N - count))
+    {
+        matrix[i, j] = a;
+        a++;
+        j++;
+    }
+    j--;
+    i--;
+
+    while (i >= (0 + count))
+    {
+        matrix[i, j] = a;
+        a++;
+        i--;
+    }
+    i++;
+    j--;
+    count++;
+
+    while (j >= (0 + count))
+    {
+        matrix[i, j] = a;
+        a++;
+        j--;
+    }
+    j++;
+    i++;
+}
+
+void FillArray(int[,] matr)
+{
+    for (int j = 0; j < N; j++)
+    {
+        for (int i = 0; i < N; i++)
+        {
+            Console.Write(string.Format("{0,2:d} ", matr[i, j]));
+        }
+        Console.WriteLine();
+    }
+}
+
+FillArray(matrix);
